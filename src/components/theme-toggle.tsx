f4607506fbb14c832a827ef54f31654c696afe06
Xyrille Navora <xyrillenavora02@gmail.com>
2026-02-12 11:28:49 +0000

    when on dark mode change the toggle to sun

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -12,11 +12,11 @@ export function ThemeToggle() {
       variant="outline"
       size="icon"
       onClick={toggleTheme}
-      className="rounded-full h-10 w-10 text-muted-foreground transition-all active:scale-95 hover:bg-background hover:text-muted-foreground"
+      className="rounded-full h-10 w-10 text-muted-foreground transition-all active:scale-95 hover:bg-transparent hover:text-muted-foreground"
       aria-label="Toggle theme"
     >
-      <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
