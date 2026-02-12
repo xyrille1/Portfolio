7d2f32f175f8c0cc41734b884f0042038ebd3fb2
Xyrille Navora <xyrillenavora02@gmail.com>
2026-02-12 12:15:51 +0000

    can we remove this icon?

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -11,25 +11,6 @@ import {
 } from "@/components/ui/toast"
 
 export function Toaster() {
-  const { toasts } = useToast()
-
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
+  // Return null to disable all toasts
+  return null;
 }
